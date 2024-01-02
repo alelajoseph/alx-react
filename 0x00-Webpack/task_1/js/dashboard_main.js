2fc2f25625abb15087a2179487db271933c48ae6
Alex Jarabi <jarabialex@gmail.com>
2024-01-02 20:03:08 +0300

    Correction

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -7,11 +7,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-$('button').click(_.debounce(updateCounter, 500));
+$('button').on('click', _.debounce(updateCounter, 500));
 
 let count = 0;
 
 function updateCounter() {
-  count++;
+  count += 1;
   $('#count').text(`${count} clicks on the button`);
 }
