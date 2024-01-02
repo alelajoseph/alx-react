47a2951318a2d7b257302ecc689e02082b4e5217
Alex Jarabi <jarabialex@gmail.com>
2024-01-02 17:55:18 +0300

    Restructured js file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,18 +2,18 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 $(document).ready(function () {
-  let count = 0;
-
-  function updateCounter() {
-    count++;
-    $('#count').text(`${count} clicks on the button`);
-  }
-
   $('body').append('<p>Holberton Dashboard</p>');
   $('body').append('<p>Dashboard data for the students</p>');
   $('body').append('<button>Click here to get started</button>');
   $('body').append('<p id="count"></p>');
   $('body').append('<p>Copyright - Holberton School</p>');
 
-  $('button').click(_.debounce(updateCounter, 1000));
+  $('button').click(_.debounce(updateCounter, 500));
+
+  let count = 0;
+
+  function updateCounter() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }
 });
