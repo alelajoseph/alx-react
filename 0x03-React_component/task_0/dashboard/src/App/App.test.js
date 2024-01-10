75e9aab2c48eecdd2af8bb96253083c0c7d2ec0b
Alex Jarabi <jarabialex@gmail.com>
2024-01-10 18:18:40 +0300

    Convert the App function into a React Class

---
+++ 0x03-React_component/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import CourseList from '../CourseList/CourseList';
+import Login from '../Login/Login';
+
+describe('App Component', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it('contains Notifications component', () => {
+    expect(wrapper.find('Notifications').exists()).toBe(true);
+  });
+
+  it('contains Header component', () => {
+    expect(wrapper.find('Header').exists()).toBe(true);
+  });
+
+  it('contains Login component', () => {
+    expect(wrapper.find('Login').exists()).toBe(true);
+  });
+
+  it('contains Footer component', () => {
+    expect(wrapper.find('Footer').exists()).toBe(true);
+  });
+
+  it('does not render CourseList by default', () => {
+    wrapper = shallow(<App isLoggedIn='false' />);
+    expect(wrapper.find(CourseList)).toHaveLength(0);
+  });
+});
+
+describe('isLoggedIn is true', () => {
+  const wrapper = shallow(<App isLoggedIn='true' />);
+  expect(wrapper.find(Login)).toHaveLength(0);
+  expect(wrapper.find(CourseList)).toHaveLength(1);
+});
