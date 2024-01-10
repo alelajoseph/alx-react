f25d5d27b2b4c640cb8bdd9ef7b3d0481835072c
Alex Jarabi <jarabialex@gmail.com>
2024-01-10 19:14:17 +0300

    Moved arrays out of the component

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -8,27 +8,21 @@ import Footer from '../Footer/Footer';
 import CourseList from '../CourseList/CourseList';
 import './App.css';
 
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+];
+
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      listCourses: [
-        { id: 1, name: 'ES6', credit: 60 },
-        { id: 2, name: 'Webpack', credit: 20 },
-        { id: 3, name: 'React', credit: 40 },
-      ],
-      listNotifications: [
-        { id: 1, type: 'default', value: 'New course available' },
-        { id: 2, type: 'urgent', value: 'New resume available' },
-        { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
-      ],
-    };
-  }
-
   render() {
     const { isLoggedIn } = this.props;
-    const { listCourses, listNotifications } = this.state;
 
     return (
       <>
