ca28d05d682141e842207e962ba6d39eea7421e3
Alex Jarabi <jarabialex@gmail.com>
2024-01-12 02:30:21 +0300

    Refactor App component to PureComponent

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Notifications from '../Notifications/Notifications';
 import { getLatestNotification } from '../utils';
@@ -8,20 +8,22 @@ import Footer from '../Footer/Footer';
 import CourseList from '../CourseList/CourseList';
 import './App.css';
 
-const listCourses = [
-  { id: 1, name: 'ES6', credit: 60 },
-  { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 },
-];
-
-const listNotifications = [
-  { id: 1, type: 'default', value: 'New course available' },
-  { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
-];
-class App extends React.Component {
+class App extends PureComponent {
   render() {
     const { isLoggedIn } = this.props;
+
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
+
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+    ];
+
     return (
       <>
         <Notifications listNotifications={listNotifications} />
